436b78abf29970d1fea6e18e40753125485db546
Doctor Jellyface <doctorjellyface@outlook.com>
2014-07-08 17:11:45 +0200

    Improved the Ubuntu 32 bit Wine compile script.
    Main changes:
     - install dependencies with official script
     - checkout to specific version
     - slightly improved comments

--- Compile-Wine/compile-wine-ubuntu-32bit.sh
+++ Compile-Wine/compile-wine-ubuntu-32bit.sh
@@ -16,13 +16,19 @@ sudo add-apt-repository -s -y ppa:ubuntu-wine/ppa
 #	Update packages
 sudo apt-get update
 
-#	Install dependencies (I have now idea what most of these packages are for
-sudo apt-get -y install git g++ gcc-multilib g++-multilib libc6-dev-i386 lib32z1-dev lib32bz2-dev xserver-xorg-dev:i386 libfreetype6-dev:i386 libmirclient-dev:i386 libosmesa6:i386 libosmesa-dev libjpeg-turbo8-dev:i386 libgl1-mesa-dev:i386 libglu1-mesa-dev:i386 libxcursor-dev:i386 libxi-dev:i386 libxrandr-dev:i386 libxinerama-dev:i386 libxcomposite-dev:i386 libdbus-1-dev:i386 libgnutls-dev:i386 libncurses5-dev:i386 libsane-dev:i386 libv4l-dev:i386 libgphoto2-6-dev:i386 liblcms2-dev:i386 liblcms1-dev:i386 gstreamer0.10-plugins-base:i386 libcapi20-dev:i386 libcups2-dev:i386 libfontconfig1-dev:i386 libgsm1-dev:i386 libtiff5-dev:i386 libmpg123-dev:i386 libopenal-dev:i386 libxrender-dev:i386 libxml2-dev:i386 libxslt1-dev:i386 libpng12-dev:i386 libssl-dev:i386 libldap2-dev:i386 unixodbc-dev:i386
+#	Install dependencies
+sudo apt-get install -y wget git g++ gcc-multilib g++-multilib
+
+#	Install build dependencies with official script
+cd
+wget http://winezeug.googlecode.com/svn/trunk/install-wine-deps.sh
+chmod +x ./install-wine-deps.sh
+sudo ./install-wine-deps.sh
 
 #	Build dependencies (again?)
-sudo apt-get -y build-dep wine1.7
+sudo apt-get build-dep -y wine1.7
 
-#	Add 2 symlinks (I think this prevents a crash)
+#	Add 2 symlinks (these symlinks are usually already there, but just to be sure)
 cd /usr/lib/i386-linux-gnu
 sudo ln -s libfreetype.so.6 libfreetype.so
 sudo ln -s libOSMesa.so.6 libOSMesa.so
@@ -30,6 +36,10 @@ sudo ln -s libOSMesa.so.6 libOSMesa.so
 #	Clone the Wine GitHub repo with the source code
 git clone git://source.winehq.org/git/wine.git ~/wine-git
 cd ~/wine-git
+#	Checkout to specific version
+#	You may want to change the version (the newest one ussually works best)
+#	You may also comment out this line if you want the newest git wine(may cause problems)
+git checkout wine-1.7.21
 
 #	Download and apply the patch we are doing all this for
 wget https://raw.githubusercontent.com/Unity3D-Wine-Support/Unity3D-on-Wine/master/Compile-Wine/WGL-ShareList-Hack.patch
@@ -39,6 +49,9 @@ cat ./WGL-ShareList-Hack.patch | patch -p1
 ./configure
 #	And actually start the build
 make
+#	havent't tried this yet:
+# make install
+#	I have no idea what it will do. It might even break your system. Use at your own risk.
 
 #	Now you should have a working (although enormous) patched Wine in your home directory
 #	Mine has a whopping 1.2 GB
