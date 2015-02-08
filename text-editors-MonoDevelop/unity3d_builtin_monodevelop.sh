1cde5f44d64d8eaca4ed3a37cd5db9b5577c1fed
Radivarig <reslav.hollos@gmail.com>
2015-02-08 21:27:24 +0100

    wineprefix path from exported variable

--- text-editors-MonoDevelop/unity3d_builtin_monodevelop.sh
+++ text-editors-MonoDevelop/unity3d_builtin_monodevelop.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 if [ -z "$1" ]; then exit 1; fi;
-#/bin/subl "`wine winepath -u "$1"`:$2" #replace subl with any other text editor, but check how its command line arguments should be formatted!!
+
+#replace subl with custom editor (watch out for its argument formatting!)
+#/bin/subl "`wine winepath -u "$1"`:$2"
+
 export WINEPREFIX=~/.local/share/wineprefixes/unity3d/
-wine ~/.local/share/wineprefixes/unity3d/drive_c/Program\ Files/Unity/MonoDevelop/bin/monodevelop.exe "$1;$2"
-exit 0 
+wine ${WINEPREFIX}drive_c/Program\ Files/Unity/MonoDevelop/bin/MonoDevelop.exe "$1;$2"
+exit 0
