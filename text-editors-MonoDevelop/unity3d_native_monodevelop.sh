0eeaf952472db904fce786aa553df1ad099680ab
Radivarig <reslav.hollos@gmail.com>
2015-02-15 22:34:23 +0100

    fix for when Unity sends built-in arguments

--- text-editors-MonoDevelop/unity3d_native_monodevelop.sh
+++ text-editors-MonoDevelop/unity3d_native_monodevelop.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
-if [ -z "$1" ]; then exit 1; fi;
+if [ -z "$1" ]; then exit 1; fi
 
-FILE_PATH=$(winepath -u "$1")
-FILE_LINE="$2"
+if [ "$1" == "--nologo" ]
+then
+	FILE_PATH="${3%;*}"
+	FILE_PATH=$(winepath -u "$FILE_PATH")
+	FILE_LINE="${3##*;}"
+else
+	FILE_PATH=$(winepath -u "$1")
+	FILE_LINE="$2"
+fi
+if [ $FILE_LINE == "-1" ]; then FILE_LINE="0"; fi
+
 SLN_DIR="${FILE_PATH%%/Assets/*}"
 SLN_PATH=$(find "$SLN_DIR" -maxdepth 1 -name "*-csharp.sln")
 SLN_NAME="${SLN_PATH#${SLN_DIR}/}"
