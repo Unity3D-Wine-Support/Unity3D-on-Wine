de7818063e664bc2d8478ae4bd8957a7b387e708
Radivarig <reslav.hollos@gmail.com>
2015-02-16 12:45:42 +0100

    added opening .sln for .js in built-in

--- text-editors-MonoDevelop/unity3d_native_monodevelop.sh
+++ text-editors-MonoDevelop/unity3d_native_monodevelop.sh
@@ -9,16 +9,10 @@ else
 	WIN_PATH="$1"
 	FILE_LINE="$2"
 fi
+
 if [ $FILE_LINE == "-1" ]; then FILE_LINE="0"; fi
 
 FILE_PATH=$(winepath -u "$WIN_PATH")
-if [[ $FILE_PATH == *".js" ]]
-then
-    export WINEPREFIX=~/.local/share/wineprefixes/unity3d/
-    wine ${WINEPREFIX}drive_c/Program\ Files/Unity/MonoDevelop/bin/MonoDevelop.exe "$WIN_PATH;$FILE_LINE"
-    exit 0
-fi
-
 SLN_DIR="${FILE_PATH%%/Assets/*}"
 SLN_PATH=$(find "$SLN_DIR" -maxdepth 1 -name "*-csharp.sln")
 SLN_NAME="${SLN_PATH#${SLN_DIR}/}"
@@ -26,6 +20,26 @@ SLN_NAME="${SLN_NAME#$?}"
 LOCAL_FILE_PATH="${FILE_PATH#$SLN_DIR}"
 LOCAL_FILE_DIR=$(dirname "$LOCAL_FILE_PATH")
 FILE_NAME="${LOCAL_FILE_PATH#${LOCAL_FILE_DIR}/}"
+
+if [[ $FILE_PATH == *".js" ]]
+then
+	SLN_NAME="${SLN_NAME/-csharp/}"
+	PREV_SLN_NAME=$(head -n 1 "${SLN_DIR}/sln_name_of_last_monodevelop_call_js")
+	
+	WIN_SLN_DIR="$(winepath -w "$SLN_DIR")"
+
+	export WINEPREFIX=~/.local/share/wineprefixes/unity3d/
+	MD_PATH="${WINEPREFIX}drive_c/Program Files/Unity/MonoDevelop/bin/MonoDevelop.exe"
+	
+	if [ "$(pidof MonoDevelop.exe)" ] && [ $PREV_SLN_NAME == $SLN_NAME ]
+	then wine "$MD_PATH" "$WIN_PATH;$FILE_LINE"
+	else wine "$MD_PATH" "${WIN_SLN_DIR}\\$SLN_NAME $WIN_PATH;$FILE_LINE"
+	fi
+
+	echo "$SLN_NAME" > "${SLN_DIR}/sln_name_of_last_monodevelop_call_js"
+    
+    exit 0
+fi
 
 COUNT="${LOCAL_FILE_PATH//[^\/]}"
 COUNT="${#COUNT}"
